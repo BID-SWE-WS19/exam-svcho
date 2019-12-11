b5cd3d0f5f2b33a1c099df6dc1f26ac162153388
Jacob Suchorabski <xcovc@icloud.com>
2019-12-11 15:04:24 +0100

    added test project

---
+++ CS/BID.SWE.EXAM.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BID.SWE.EXAM.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
