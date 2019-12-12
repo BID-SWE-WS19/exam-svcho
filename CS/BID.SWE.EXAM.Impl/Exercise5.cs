21757f57598ecc3e7ad1684c83335181c3e90a5a
Jacob Suchorabski <xcovc@icloud.com>
2019-12-12 16:32:15 +0100

    ex 5

--- CS/BID.SWE.EXAM.Impl/Exercise5.cs
+++ CS/BID.SWE.EXAM.Impl/Exercise5.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
 using BID.SWE1.Exam.Interfaces;
 
 namespace BID.SWE.EXAM.Impl
@@ -10,6 +12,30 @@ namespace BID.SWE.EXAM.Impl
         public object Method1(string str, int i)
         {
             List<string> list = new List<string>();
+
+            int byteCount = 0;
+            byte[] bytes = new byte[256];
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork,
+        SocketType.Stream,
+        ProtocolType.Tcp);
+
+            clientSocket.Connect(str, i);
+
+            byteCount = clientSocket.Receive(bytes, 0, clientSocket.Available,
+                                   SocketFlags.None);
+
+            if (byteCount > 0)
+            {
+                string[] receivedLines = Encoding.UTF8.GetString(bytes, 0, bytes.Length).Split("\n");
+
+                foreach (string line in receivedLines)
+                {
+                    list.Add(line);
+                }
+            }
+
+            return list;
         }
     }
 }
